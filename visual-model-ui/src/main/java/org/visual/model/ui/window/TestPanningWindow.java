9cd44a96c63ce926fc372087bb324148e3f8e4b8
DaiYuANg <dai_yuang@icloud.com>
2024-01-12 15:07:14 +0800

    update panning window

--- visual-model-ui/src/main/java/org/visual/model/ui/window/TestPanningWindow.java
+++ visual-model-ui/src/main/java/org/visual/model/ui/window/TestPanningWindow.java
@@ -7,12 +7,13 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import lombok.val;
 import org.jetbrains.annotations.NotNull;
+import org.visual.model.ui.layout.GirdBackground;
 
-public class Test extends Application {
+public class TestPanningWindow extends Application {
     @Override
     public void start(@NotNull Stage stage) throws Exception {
         val panningWindow = new PanningWindow();
-        panningWindow.getChildren().add(new Button("test"));
+        panningWindow.getChildren().add(new GirdBackground());
         val scene = new Scene(new StackPane(panningWindow), 600, 600);
         scene.getStylesheets().add("/window.css");
         stage.setScene(scene);
